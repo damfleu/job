7d2414ec9c7e70ea62206ebfe2904aa7bd74b8fc
Damien Fleurisson <damien.fleurisson@pm.me>
2026-04-11 15:57:04 +0200

    feat(cli): job show and job log commands

---
+++ internal/cli/log.go
@@ -0,0 +1,44 @@
+package cli
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"job/internal/core"
+)
+
+var logPath bool
+
+var logCmd = &cobra.Command{
+	Use:   "log <key>",
+	Short: "Display log output for a job",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		j, err := core.ResolveKey(globalDB, args[0])
+		if err != nil {
+			return err
+		}
+
+		if logPath {
+			fmt.Println(j.LogFile)
+			return nil
+		}
+
+		f, err := os.Open(j.LogFile)
+		if err != nil {
+			return fmt.Errorf("opening log: %w", err)
+		}
+		defer f.Close()
+
+		_, err = io.Copy(os.Stdout, f)
+		return err
+	},
+}
+
+func init() {
+	logCmd.Flags().BoolVarP(&logPath, "path", "p", false, "print log file path only")
+	rootCmd.AddCommand(logCmd)
+}
---
+++ internal/cli/show.go
@@ -0,0 +1,77 @@
+package cli
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/spf13/cobra"
+
+	"job/internal/core"
+	"job/internal/model"
+)
+
+var showCmd = &cobra.Command{
+	Use:   "show <key>",
+	Short: "Show full details for a job",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		j, err := core.ResolveKey(globalDB, args[0])
+		if err != nil {
+			return err
+		}
+		printJob(j)
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(showCmd)
+}
+
+func printJob(j *model.Job) {
+	field := func(label, value string) {
+		fmt.Printf("%-12s %s\n", label+":", value)
+	}
+
+	field("Key", j.Key)
+	if j.Alias != "" {
+		field("Alias", j.Alias)
+	}
+	field("Command", strings.Join(j.Command, " "))
+	field("WorkDir", j.WorkDir)
+	field("Log", j.LogFile)
+	field("Status", string(j.Status))
+	if j.Reason != "" {
+		field("Reason", string(j.Reason))
+	}
+	if j.ExitCode != nil {
+		field("Exit code", fmt.Sprintf("%d", *j.ExitCode))
+	}
+	if j.PID != 0 {
+		field("PID", fmt.Sprintf("%d", j.PID))
+	}
+	field("Host", j.Hostname)
+	field("User", j.Username)
+	field("Created", formatTime(j.CreatedAt))
+	if j.StartedAt != nil {
+		field("Started", formatTime(*j.StartedAt))
+	}
+	if j.StoppedAt != nil {
+		field("Stopped", formatTime(*j.StoppedAt))
+		field("Duration", j.StoppedAt.Sub(j.CreatedAt).Round(time.Millisecond).String())
+	}
+	if len(j.Deps) > 0 {
+		for i, d := range j.Deps {
+			label := "Dep"
+			if i > 0 {
+				label = ""
+			}
+			field(label, fmt.Sprintf("%s (%s)", d.Key, d.Kind))
+		}
+	}
+}
+
+func formatTime(t time.Time) string {
+	return t.Local().Format("2006-01-02 15:04:05")
+}
